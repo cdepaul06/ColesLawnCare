89fba6c7afd317fdf59baf3a83596ea3f1f8e6de
Chris <73894631+cdepaul06@users.noreply.github.com>
2023-07-02 14:09:37 -0400

    mobile adjustments - WIP

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -10,19 +10,38 @@ import { BusinessLogo } from "../../assets";
 
 const Navigation = ({}) => {
   return (
-    <nav className='flex justify-between items-center bg-white p-5 border-b border-gray-300 h-15'>
+    <nav className='flex flex-wrap justify-between items-center bg-white p-5 border-b border-gray-300'>
       <Link
         to='/'
-        className='flex items-center gap-2 pl-10'
+        className='flex items-center gap-2 pl-2 w-full md:w-auto md:pl-10'
         onClick={() => window.scrollTo(0, 0)}
       >
         <img
           src={BusinessLogo}
           alt='logo'
-          className='w-13 h-13 object-contain'
+          className='w-15 h-15 sm:w-13 sm:h-13 object-contain md:w-auto md:h-auto'
         />
       </Link>
-      <ul className='flex items-center space-x-4 ml-auto'>
+      <input type='checkbox' id='menu-toggle' className='hidden' />
+      <label
+        htmlFor='menu-toggle'
+        className='block cursor-pointer md:hidden ml-auto'
+      >
+        <svg
+          className='fill-current text-gray-900'
+          xmlns='http://www.w3.org/2000/svg'
+          width='20'
+          height='20'
+          viewBox='0 0 20 20'
+        >
+          <title>menu</title>
+          <path d='M0 3.5A1.5 1.5 0 011.5 2h17a1.5 1.5 0 110 3h-17A1.5 1.5 0 010 3.5zM1.5 9a1.5 1.5 0 011.5-1.5h17a1.5 1.5 0 110 3h-17A1.5 1.5 0 011.5 9zm0 5a1.5 1.5 0 011.5-1.5h17a1.5 1.5 0 110 3h-17A1.5 1.5 0 011.5 14z' />
+        </svg>
+      </label>
+      <ul
+        className='flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-4 ml-auto md:ml-0 md:items-center md:justify-end w-full md:w-auto hidden md:flex'
+        id='menu'
+      >
         {navLinks.map((link) => (
           <li key={link.id}>
             <a

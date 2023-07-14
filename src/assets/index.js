e20eed6044ea2ab14eaa2fbc2533c8eb07f2fc00
Chris <73894631+cdepaul06@users.noreply.github.com>
2023-07-14 18:50:02 -0400

    Added some additional comments/instructions

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,5 @@
+// * This file is used to import all the images in the assets folder
+// * You simply copy/paste or drag+drop the images into the assets folder and follow the same format as below
 import BusinessLogo from "./BusinessLogo.png";
 import lawn1 from "./lawn1.jpg";
 import lawn2 from "./lawn2.jpg";
@@ -7,4 +9,5 @@ import lawn5 from "./lawn5.jpg";
 import lawn6 from "./lawn6.jpg";
 import lawn7webp from "./lawn7.webp";
 
+// * Make sure to export any new images you add to the assets folder
 export { BusinessLogo, lawn1, lawn2, lawn3, lawn4, lawn5, lawn6, lawn7webp };
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,6 +1,8 @@
+// * You can CTRL + CLICK on the '../assets' to see the files in the folder and add/remove as needed.
+// * Obviously you will need to import them here to be used in the app.
 import { lawn1, lawn2, lawn3, lawn4, lawn5, lawn6, lawn7webp } from "../assets";
 
-// You can add additional links to the navigation bar here.
+// * You can add additional links to the navigation bar here.
 const navLinks = [
   {
     id: "services",
@@ -16,14 +18,22 @@ const navLinks = [
   },
 ];
 
-// You can add additional content to the About section here.
+// * You can add additional content to the About section here.
 const aboutContent = {
   id: "about-content",
   name: "Cole's Lawn Care",
   description:
-    "This is a wall of text. This is a wall of text. This is a wall of text. This is a wall of text. This is a wall of text. This is a wall of text. This is a wall of text. This is a wall of text. This is a wall of text. This is a wall of text. This is a wall of text. This is a wall of text. This is a wall of text. This is a wall of text. This is a wall of text. This is a wall of text. This is a wall of text. This is a wall of text. This is a wall of text. This is a wall of text. This is a wall of text. This is a wall of text. This is a wall of text. This is a wall of text. ",
+    "This is a wall of text. This is a wall of text. This is a wall of text. " +
+    "This is a wall of text. This is a wall of text. This is a wall of text. " +
+    "This is a wall of text. This is a wall of text. This is a wall of text. " +
+    "This is a wall of text. This is a wall of text. This is a wall of text. " +
+    "This is a wall of text. This is a wall of text. This is a wall of text. " +
+    "This is a wall of text. This is a wall of text. This is a wall of text. " +
+    "This is a wall of text. This is a wall of text. This is a wall of text. " +
+    "This is a wall of text. This is a wall of text. This is a wall of text. ",
 };
 
+// * Use the imports from above as the src for the images.
 const lawnImages = [
   {
     id: "lawn1-1",
@@ -63,6 +73,8 @@ const aboutImage = {
   alt: "lawn7webp",
 };
 
+// * This is where you can add/remove any of the services you want listed on the website.
+// * Each one must have a unique ID. Easiest to just do service-1, service-2, etc.
 const servicesContent = [
   {
     id: "service-1",
@@ -81,14 +93,17 @@ const servicesContent = [
     image: lawn1,
     details: ["Mulching", "Trimming", "Other things"],
   },
-  {
-    id: "service-3",
-    title: "Whatever you want",
-    description:
-      "We will remove snow from your driveway and make it look nice.",
-    image: "lawn3",
-    details: ["Whatever you want.", "Whatever you want.", "Whatever you want."],
-  },
+  // * You can uncomment this if you want to add additional services, if you offer more than 3 you can just
+  // * copy and paste the above service and change the ID and content.
+  // {
+  //   id: "service-3",
+  //   title: "Whatever you want",
+  //   description:
+  //     "We will remove snow from your driveway and make it look nice.",
+  //   image: "lawn3",
+  //   details: ["Whatever you want.", "Whatever you want.", "Whatever you want."],
+  // },
 ];
 
+// If for whatever reason you add more constants to the global index you need to export them here.
 export { navLinks, aboutContent, lawnImages, servicesContent, aboutImage };

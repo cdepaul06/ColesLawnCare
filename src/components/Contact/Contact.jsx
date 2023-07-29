17d7ddbc89564826921785462bb9a578ab30d1da
Chris <73894631+cdepaul06@users.noreply.github.com>
2023-07-29 11:48:51 -0400

    oops

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -31,9 +31,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: VITE_APP_NAME,
+          to_name: import.meta.env.VITE_APP_NAME,
           from_email: form.email,
-          to_email: VITE_APP_EMAIL,
+          to_email: import.meta.env.VITE_APP_EMAIL,
           message: form.message,
         },
         import.meta.env.VITE_APP_PUBLIC_KEY
